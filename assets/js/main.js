381573fec918165123facc4afef089568f52aa31
yuzu fell into darkness <160171833+ywxzyy@users.noreply.github.com>
2024-08-12 12:48:28 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,4 +37,4 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__data`, {origin: 'top', delay: 400})
 sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
-sr.reveal(`.home__footer`, {origin: 'bottom', delay: 800})
+sr.reveal(`.home__footer`, {origin: 'bottom', delay: 450})
